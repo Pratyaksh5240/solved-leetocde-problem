c6531213ec65a59d5495c397611c3c83079e6046
Pratyaksh Soni29 <202421724+Pratyaksh5240@users.noreply.github.com>
2026-06-06 08:10:57 +0530

    Time: 8 ms (28.93%), Space: 13 MB (99.38%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+
+        q = deque()
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        order = []
+
+        while q:
+            node = q.popleft()
+            order.append(node)
+
+            for nei in graph[node]:
+                indegree[nei] -= 1
+
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        return order if len(order) == numCourses else []
