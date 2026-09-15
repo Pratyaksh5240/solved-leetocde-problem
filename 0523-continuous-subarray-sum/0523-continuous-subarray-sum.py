614602ed2c5066de38f22ff80050ec56bd63d71c
Pratyaksh Soni29 <202421724+Pratyaksh5240@users.noreply.github.com>
2026-09-15 22:38:49 +0530

    Time: 71 ms (9.45%), Space: 31.5 MB (38.01%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        remainder = {0: -1}
+
+        total = 0
+
+        for i in range(len(nums)):
+            total += nums[i]
+
+            rem = total % k
+
+            if rem in remainder:
+                if i - remainder[rem] >= 2:
+                    return True
+            else:
+                remainder[rem] = i
+
+        return False
